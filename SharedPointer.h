8b047259c618a4f02381fe6ff935c976125b500d
nw316 <nw316@ic.ac.uk>
2017-08-18 21:01:32 +0100

    ready to debug

--- SharedPointer.h
+++ SharedPointer.h
@@ -6,13 +6,13 @@ class SharedPointer {
 public:
 
 	//default constructor default initialises the pointer to null
-	SharedPointer();
+	explicit SharedPointer();
 
 	//wraps the pointer in the smart pointer
-	SharedPointer(T*);
+	explicit SharedPointer(T*);
 
 	//copy constructor and assignment operator
-	SharedPointer(const SharedPointer&);
+	explicit SharedPointer(const SharedPointer&);
 	SharedPointer& operator=(const SharedPointer&);
 
 	~SharedPointer();
@@ -26,11 +26,8 @@ public:
 	//dereference an objects member
 	T* operator->() const;
 
-	//deletes underlying object
-	void reset();
-
-	//deletes underlying object and assigns ownership to the new pointer
-	void reset(T*);
+	//switches ownership to new pointer
+	void reset(T* o_ptr = nullptr);
 
 	//return bare pointer
 	T* get() const;
@@ -38,30 +35,86 @@ public:
 private:
 	int* reference_count;
 	T* ptr;
+
+	//releases ownership of the shared pointer on ptr 
+	void release();
 };
 
 template <class T>
-SharedPointer<T>::SharedPointer() : reference_count(new int(0)), ptr(nullptr) {}
+SharedPointer<T>::SharedPointer()
+	: reference_count(new int(1)),
+		ptr(nullptr) {}
 
 template <class T>
 SharedPointer<T>::SharedPointer(T* ptr)
-	: reference_count(new int(0)),
-		ptr(ptr) {
-	//if ptr is not null then increment the reference count
-	if (!ptr) {
-		(*reference_count)++;
-	}
-}
+	: reference_count(new int(1)),
+		ptr(ptr) {}
 
 template <class T>
 SharedPointer<T>::SharedPointer(const SharedPointer& shared_ptr)
 	: reference_count(shared_ptr.reference_count),
-		ptr(shared_ptr.ptr) {
-	//is shared_ptr's underlying pointer is not null then increment the reference count
-	if (!shared_ptr.ptr) {
-		++*reference_count;
+		ptr(shared_ptr.ptr) {}
+
+template <class T>
+SharedPointer<T>& SharedPointer<T>::operator=(const SharedPointer& rhs) {
+  //check for self assignmenet
+	if (&rhs == this) {
+		return *this;
 	}
+
+	//increase ref count of rhs (prevents it being destroyed if this is destroyed)
+	++(*rhs.reference_count);
+
+	release();
+
+	ptr = rhs.ptr;
+	reference_count = rhs.reference_count;
+	return *this;
 }
+
+template <class T>
+SharedPointer<T>::~SharedPointer() {
+	release();
+}
+
+
+template <class T>
+void SharedPointer<T>::release() {
+	//if reference count drops to 0 then free memory
+	if (--(*reference_count) == 0) {
+		delete reference_count;
+		delete ptr;
+	}
+
+	//set pointers to null
+	reference_count = nullptr;
+	ptr = nullptr;
+}
+
+template <class T>
+SharedPointer<T>::operator bool() const {
+	return ptr;
+}
+
+template <class T>
+T* SharedPointer<T>::operator->() const {
+	return ptr;
+}
+
+template <class T>
+T& SharedPointer<T>::operator*() const  {
+	return *ptr;
+}
+
+template <class T>
+void SharedPointer<T>::reset(T* other_ptr) {
+
+	release();
+
+	ptr = other_ptr;
+	reference_count = new int(1);
+}
+
 
 
 #endif
